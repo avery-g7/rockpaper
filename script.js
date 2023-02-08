2632a626a398a2aea54a4c26a45587b0df013cf4
Avery <averygadd7@gmail.com>
2023-02-08 16:28:31 -0700

    make pseudoscript and variables

---
+++ script.js
@@ -0,0 +1,17 @@
+/*
+make function that chooses 3 options for computer randomly
+make prompt ask to input choice
+make rock > scissors > paper > rock
+make a function that displays a win or lose message
+make function that plays rps for a round
+make a loop that goes until one gets to 5 Ws*/
+
+
+/*computer function */
+let system = prompt('Rock, Paper, Scissors').toLowerCase;
+let rock = parseInt(1);
+let paper = parseInt(2);
+let scissors = parseInt(3);
+function getComputerChoice() {
+
+}
