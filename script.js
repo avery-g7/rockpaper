cf8b9f0a36cd89d29fd14f1fd12c9894ffabb2a6
Avery <averygadd7@gmail.com>
2023-02-09 16:19:01 -0700

    fixed computer choice to be random

--- script.js
+++ script.js
@@ -7,30 +7,45 @@ make function that plays rps for a round
 make a loop that goes until one gets to 5 Ws*/
 
 /* input choice prompt */
-let system = prompt('Rock, Paper, Scissors').toLowerCase;
+let system = prompt('Rock, Paper, Scissors');
 
 let playerScore = 0
 let computerScore = 0
 
-let rock = parseInt(0);
-let paper = parseInt(1);
-let scissors = parseInt(2);
-
+function random(number) {
+    return Math.floor(Math.random() * number + 1);
+}
 /* computer function */
 function getComputerChoice() {
-    let choice = ('rock', 'paper', 'scissors');
-    return Math.floor(Math.random()*choice.length);
+    let choice = random(3);
+    let compChoice = '';
+    switch(choice) {
+        case 1:
+            compChoice = 'rock'
+            break;
+        case 2:
+            compChoice = 'paper'
+            break;
+        case 3:
+            compChoice = 'scissors'
+            break;
+    }
+    return compChoice;
 }
 
+
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == rock && computerSelection == scissors ||
-    playerSelection == paper && computerSelection == rock ||
-    playerSelection == scissors && computerSelection == paper){
-        return ('Easy dub'+playerSelection+'beats'+computerSelection)
-    } else if (playerSelection == computerSelection){
-        return ("It's a draw, you both chose"+playerSelection)
+    if (playerSelection == 'rock' && computerSelection == 'scissors' ||
+    playerSelection == 'paper' && computerSelection == 'rock' ||
+    playerSelection == 'scissors' && computerSelection == 'paper'){
+        playerScore += 1
+        return ('Easy dub '+playerSelection+' beats '+computerSelection)
+    } else if (playerSelection === computerSelection){
+        return ("It's a draw, you both chose "+playerSelection)
     } else {
-        return (computerSelection+'beats'+playerSelection+'bro really lost to a computer')
+        computerScore += 1
+        return (computerSelection+' beats '+playerSelection+' bro really lost to a computer')
+        
     }
     
 }
