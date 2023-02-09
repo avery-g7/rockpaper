b233943fb5cedc5fd60bd23c23381658d4d8b7ad
Avery <averygadd7@gmail.com>
2023-02-08 18:54:01 -0700

    added random generator for computer

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 /*
-make function that chooses 3 options for computer randomly
-make prompt ask to input choice
+YES make function that chooses 3 options for computer randomly
+YES make prompt ask to input choice
 make rock > scissors > paper > rock
 make a function that displays a win or lose message
 make function that plays rps for a round
 make a loop that goes until one gets to 5 Ws*/
 
-
-/*computer function */
+/* input choice prompt */
 let system = prompt('Rock, Paper, Scissors').toLowerCase;
-let rock = parseInt(1);
-let paper = parseInt(2);
-let scissors = parseInt(3);
-function getComputerChoice() {
 
+let playerScore = 0
+let computerScore = 0
+
+let rock = parseInt(0);
+let paper = parseInt(1);
+let scissors = parseInt(2);
+
+/* computer function */
+function getComputerChoice() {
+    let choice = (rock, paper, scissors);
+    return Math.floor(Math.random()*3);
 }
+
+console.log(getComputerChoice());
