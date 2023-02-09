dcc6933366ca7510545e192845aac0beaf877e13
Avery <averygadd7@gmail.com>
2023-02-09 12:57:58 -0700

    added round function and win outcome

--- script.js
+++ script.js
@@ -18,8 +18,23 @@ let scissors = parseInt(2);
 
 /* computer function */
 function getComputerChoice() {
-    let choice = (rock, paper, scissors);
-    return Math.floor(Math.random()*3);
+    let choice = ('rock', 'paper', 'scissors');
+    return Math.floor(Math.random()*choice.length);
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == rock && computerSelection == scissors ||
+    playerSelection == paper && computerSelection == rock ||
+    playerSelection == scissors && computerSelection == paper){
+        return ('Easy dub'+playerSelection+'beats'+computerSelection)
+    } else if (playerSelection == computerSelection){
+        return ("It's a draw, you both chose"+playerSelection)
+    } else {
+        return (computerSelection+'beats'+playerSelection+'bro really lost to a computer')
+    }
+    
+}
+
+const playerSelection = system;
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
