ab28a3ea3e56130b674856f3e841a98ed2a21806
Avery <averygadd7@gmail.com>
2023-02-09 23:31:08 -0700

    added loop to code until someone wins

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 /*
 YES make function that chooses 3 options for computer randomly
 YES make prompt ask to input choice
-make rock > scissors > paper > rock
-make a function that displays a win or lose message
-make function that plays rps for a round
-make a loop that goes until one gets to 5 Ws*/
+YES make rock > scissors > paper > rock
+YES make a function that displays a win or lose message
+YES make function that plays rps for a round
+YES make a loop that goes until one gets to 5 Ws*/
 
-/* input choice prompt */
-let system = prompt('Rock, Paper, Scissors');
 
 let playerScore = 0
 let computerScore = 0
@@ -19,6 +17,7 @@ function random(number) {
 function getComputerChoice() {
     let choice = random(3);
     let compChoice = '';
+
     switch(choice) {
         case 1:
             compChoice = 'rock'
@@ -33,23 +32,41 @@ function getComputerChoice() {
     return compChoice;
 }
 
-
+/* round function and win or loss */
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == 'rock' && computerSelection == 'scissors' ||
     playerSelection == 'paper' && computerSelection == 'rock' ||
     playerSelection == 'scissors' && computerSelection == 'paper'){
-        playerScore += 1
+        playerScore++
         return ('Easy dub '+playerSelection+' beats '+computerSelection)
     } else if (playerSelection === computerSelection){
         return ("It's a draw, you both chose "+playerSelection)
     } else {
-        computerScore += 1
+        computerScore++
         return (computerSelection+' beats '+playerSelection+' bro really lost to a computer')
         
     }
     
 }
 
-const playerSelection = system;
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+
+/* loop for the game */
+function game() {
+    for (let i = 0;playerScore < 5 && computerScore < 5; i++ ) {
+        let playerSelection = prompt('Rock, Paper, Scissors').toLowerCase();
+        let computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(`Player score: ${playerScore} Computer score: ${computerScore}`)
+
+    }
+}
+
+
+game();
+
+/* game over message */
+if (playerScore == 5) {
+    console.log('You got to 5 first Big dubs');
+} else if (computerScore == 5) {
+    console.log('Computer got to 5 first Holy L');
+}
